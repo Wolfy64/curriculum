647afa70f61f9c0057cbed47bba3ade04af38d73
David <ledavid64@gmail.com>
2018-06-09 15:35:19 -0700

    Solve problem js2-03

--- curriculum/js2/3.js
+++ curriculum/js2/3.js
@@ -5,11 +5,16 @@
  * @param {array} a
  * @returns {array}
  */
+const primeFn = (a, i = 2) => {
+  if (a === i ) return a;
+  if (a % i === 0 || a === 1) return 1;
+  return primeFn(a, i+1);
+};
 
 const solution = (a) => {
-  return [];
+  return a.map(e => primeFn(e));
 };
 
 module.exports = {
-  solution,
+  solution
 };
