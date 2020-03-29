0555aca66b85f50b0c07cedcb6d6b2b8862432e8
David De Wulf <ledavid64@gmail.com>
2020-03-29 15:37:05 -0700

    solved

--- js3/1.js
+++ js3/1.js
@@ -4,8 +4,11 @@
  * @returns {array} arr
  */
 
-const solution = (arr, obj) => {
-  return []
+const solution = (arr, obj, i=0, result=[]) => {
+  if(i === arr.length) return result;
+  const key = arr[i];
+  if(obj[key]) result.push(obj[key]);
+  return solution(arr, obj, i+1, result)
 }
 
 module.exports = {
