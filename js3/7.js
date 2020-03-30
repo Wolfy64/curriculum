0261dcd152bd984cf5f5413f91f3837c67931dd7
David De Wulf <ledavid64@gmail.com>
2020-03-29 18:36:19 -0700

    solved

--- js3/7.js
+++ js3/7.js
@@ -2,14 +2,30 @@
  * write a map function for objects
  * @param {callback} cb
  * @returns {number}
-*/
+ *
+ * const info = {
+ *   ironman: 'arrogant',
+ *   spiderman: 'naive',
+ *   hulk: 'strong',
+ * }
+ *
+ * const result = info.map( (key, value, i) => {
+ *   return key + i + value
+ * })
+ * console.log(result)
+ *
+ * will print out ['ironman0arrogant', 'spiderman1naive', 'hulk2strong']
+ */
 
 const solution = () => {
-  Object.prototype.map = function (cb) {
-    return []
-  }
-}
+  Object.prototype.map = function(cb, i=0, result=[]) {
+    if(i === Object.keys(this).length) return result;
+    const [key, value] = Object.entries(this)[i];
+    result.push(cb(key, value, i))
+    return this.map(cb, i+1, result);
+  };
+};
 
 module.exports = {
   solution
-}
+};
