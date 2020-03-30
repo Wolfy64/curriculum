1f0d4a6202545fde8c1cc2d62ee5847e23dca859
David De Wulf <ledavid64@gmail.com>
2020-03-29 19:23:52 -0700

    solved

--- js3/8.js
+++ js3/8.js
@@ -5,12 +5,47 @@
  * @param {object} obj
  * @param {number} num (millieseconds)
  * @call each function value of the object, millieseconds after each other
-*/
+ */
 
-const solution = (obj, num) => {
+// const solution = (obj, num, i=0) => {
+//   const keys = Object.keys(obj);
+//   const key = keys[i];
+//   if(i === keys.length) return;
+//   if(i === 0){
+//     obj[key](key)
+//     return solution(obj, num, i + 1);
+//   }
+//   setTimeout(() => {
+//     obj[key](key)
+//     return solution(obj, num, i + 1);
+//   }, num)
+// }
+const solution = (obj, num, i = 0, isWaiting = false) => {
+  const keys = Object.keys(obj);
+  const key = keys[i];
+  if (i === keys.length) return;
+  // if(!isWaiting){
+  //   obj[key](key);
+  //   isWaiting = true;
+  //   setTimeout(() => {
+  //     isWaiting = false;
+  //     return solution(obj, num, i + 1, isWaiting);
+  //   }, num);
+  // }
 
-}
+  if(!isWaiting){
+    obj[key](key);
+    isWaiting = true;
+  }
+
+
+
+  setTimeout(() => {
+    isWaiting = false;
+    return solution(obj, num, i + 1, isWaiting);
+  }, num);
+};
 
 module.exports = {
   solution
-}
+};
