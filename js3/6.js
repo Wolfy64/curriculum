5baee6a302d34141be0e75d0e511fb51d4938e64
David De Wulf <ledavid64@gmail.com>
2020-03-29 18:25:50 -0700

    solved

--- js3/6.js
+++ js3/6.js
@@ -2,12 +2,16 @@
  * write a function that takes in an array of numbers, and returns a new array of all duplicate numbers
  * @param {array} arr
  * @returns {array}
-*/
+ */
 
-const solution = (arr) => {
-  return []
-}
+const solution = (arr, i = 0, map = {}, result = []) => {
+  const value = arr[i];
+  if (i === arr.length) return result;
+  map[value] = map[value] + 1 || 0;
+  if(map[value] === 1) result.push(value);
+  return solution(arr, i + 1, map, result);
+};
 
 module.exports = {
   solution
-}
+};
