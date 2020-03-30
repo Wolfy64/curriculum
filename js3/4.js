a089b007187352f1d06f5c4641d3df78661c7bef
David De Wulf <ledavid64@gmail.com>
2020-03-29 17:25:48 -0700

    solved

--- js3/4.js
+++ js3/4.js
@@ -5,8 +5,19 @@
  * @returns {boolean}
  */
 
+// solution([1,2,22,333,23], 25)   // returns true
+// solution([1,2,22,333,23], 24)   // returns true
 const solution = (arr, num) => {
-  return true
+  if(num === 0 && arr.length === 0) return false;
+  for(let i = 0; i < arr.length; i++){
+    for(let j = i+1; j < arr.length; j++){
+      if ( arr[i] + arr[j] === num) {
+        return true;
+      }
+    }
+  }
+
+  return false
 }
 
 module.exports = {
